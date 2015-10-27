a0c66e36ed4d6b65665ed56a626757200b04e160
Rudi <flaing12@gmail.com>
2015-10-27 23:54:20 +0800

    home w64 configuration

--- init.el
+++ init.el
@@ -1,34 +1,35 @@
-(setq visible-bell 1)
-(require 'package) ;; You might already have this line
+;; Define package repositories
+(require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
-(load-theme 'tsdh-dark)
-(global-linum-mode t)
-(helm-mode t)
-(global-company-mode)
-;; cider-company
-(add-hook 'cider-repl-mode-hook #'company-mode)
-(add-hook 'cider-mode-hook #'company-mode)
 
+;; Load and activate emacs packages. Do this first so that the
+;; packages are loaded before you start trying to modify them.
+;; This also sets the load path.
+
+;; Download the ELPA archive description if needed.
+;; This informs Emacs about the latest versions of all packages, and
+;; makes them available for download.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(global-linum-mode t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (irony company-irony csharp-mode fsharp-mode scala-mode2 ac-alchemist eshell-z exec-path-from-shell helm better-defaults jvm-mode sqlup-mode clojure-mode clojure-mode-extra-font-locking omn-mode quickrun jsx-mode sml-mode haskell-mode nrepl-sync))))
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(helm-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Courier" :width medium)))))
-
-;; haskell indentation
-(add-hook 'turn-on-haskell-indentation #'haskell-mode)
-
+ '(default ((t (:family "Courier" :width normal))))
+ )
